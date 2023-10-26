e11c82b291b98602e0abf062151ab0fc0d8faa66
pavankumarmurugan <d23124670@mytudublin.ie>
2023-10-26 04:05:23 +0100

    Changes for email confirmation

--- profile-service/src/main/java/com/profile/service/service/UserService.java
+++ profile-service/src/main/java/com/profile/service/service/UserService.java
@@ -1,14 +1,16 @@
 package com.profile.service.service;
 
-import com.profile.service.controller.UserSignInResponse;
 import com.profile.service.request.UserRegisterRequest;
 import com.profile.service.request.UserSignInRequest;
 import com.profile.service.response.UserRegisterResponse;
+import com.profile.service.response.UserSignInResponse;
 
 public interface UserService {
 	
 	UserRegisterResponse registerUser(UserRegisterRequest userRegisterRequest);
 	
 	UserSignInResponse userSignIn(UserSignInRequest userSignInRequest);
+	
+	String confirmToken(String token);
 	
 }
